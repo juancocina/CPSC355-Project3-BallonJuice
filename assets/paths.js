f542ac831ed4d6422de55e334b95731bc396d865
juancocina <juan_cocina@csu.fullerton.edu>
2020-12-09 22:52:46 -0800

    Update paths.js

--- assets/paths.js
+++ assets/paths.js
@@ -1,12 +1,13 @@
+var srcBalloon;
 class GeneratePaths {
-  constructor(balloon, barray){
+  constructor(firstballoon, barray){
     this.doesExist = false;
     this.isJuice = false;
-    this.srcBalloon = balloon;
+    srcBalloon = Object.assign({},firstballoon);
     this.listOfBalloons = barray;
 
     this.graph = new Graph();
-    this.graph.addVertex(balloon);
+    this.graph.addVertex(srcBalloon);
   }
 
   reset(){
@@ -15,184 +16,237 @@ class GeneratePaths {
   }
 
   //once done, the manager will call done(), which will call DJS in graph.js
-  done(){
-    this.graph.dfs();
+  done(barray){
+    this.kappa = barray;
+    this.graph.dfs(this.kappa[0]);
   }
 
   run(balloot){ //one call to "run()" = 1 pass
-    //initialize array
-    this.srcBalloon = balloot;
-    console.log(this.srcBalloon);
-    let destBalloon = balloot;
-    console.log("source balloon has changed");
+    let destBalloon =  Object.assign({}, balloot); //pass values to destination balloon
+    //this.listOfBalloons.push(destBalloon);
+    //console.log("start of run");
+    //console.log(balloot);
+    //console.log(destBalloon)
 
-    //first check if balloon is empty/errored
-    if(this.srcBalloon.dark == 0 && this.srcBalloon.pale == 0 && this.srcBalloon.veined == 0){
-      console.log("error, all values at 0");
+    //check if the balloon is empty
+    if(destBalloon.dark == 0 && destBalloon.pale == 0 && destBalloon.veined == 0){
+      console.log("error, balloon is empty (all 0s)");
     }
     //check if we have the balloon we want
-    if(this.srcBalloon.dark == 3 && this.srcBalloon.pale == 13 && this.srcBalloon.veined == 3){
+    if(destBalloon.dark == 3 && destBalloon.pale == 13 && destBalloon.veined == 3){
       this.isJuice = true;
+      addVertex(destBalloon); //add to graph
+      this.listOfBalloons.push(destBalloon); //add to list being used by manager
 
-      addVertex(this.srcBalloon); //add to graph
-      this.listOfBalloons.push(this.srcBalloon); //add to list being used by manager
-
-      console.log(this.srcBalloon);
-    }
-//this section will be modifying dark and pale------------------------------------
-    //first d <---- p
-    if(this.srcBalloon.dark + this.srcBalloon.pale > 19){ //error check
-      console.log("error, check sizes of dark and pale")
-      console.log(this.srcBalloon.dark);
-      console.log(this.srcBalloon.pale);
-    }
-    if(destBalloon.dark + destBalloon.pale <= 19){ //1.move all to D from P
-      console.log("move all to D from P")
-      destBalloon.dark = destBalloon.dark + destBalloon.pale;
-      destBalloon.pale = destBalloon.pale - destBalloon.pale;
-
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
       console.log(destBalloon);
-      console.log(this.srcBalloon);
+    }
+/*  this section will modify dark and pale -----------------------------------------------*/
+    //first d <---- p
+    if(destBalloon.dark + destBalloon.pale > 19){
+      console.log("error, check sizes of dark and pale");
+      console.log(destBalloon.dark);
+      console.log(destBalloon.pale);
+    }
+    //1.move all to D from p
+    if(destBalloon.dark + destBalloon.pale <= 19){
+      //console.log("move all to D from P");
+      destBalloon.dark = destBalloon.dark + destBalloon.pale;
+      destBalloon.pale = destBalloon.pale - destBalloon.pale
+
+      if(destBalloon == balloot){// if it equals the same balloon after modding, dont push it
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon = this.srcBalloon;
+      destBalloon =  Object.assign({}, balloot);
     }
-
     //next d ----> p
-    if(this.srcBalloon.dark + this.srcBalloon.pale <= 13){//2. move all to P from D
-      console.log("move all from P to D");
+    //2. move all to P from D
+    if(destBalloon.dark + destBalloon.pale <= 13){
+      //console.log("move all from P to D");
+      //console.log(destBalloon);
       destBalloon.pale = destBalloon.dark + destBalloon.pale;
       destBalloon.dark = destBalloon.dark - destBalloon.dark;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
-      console.log(destBalloon);
-      console.log(this.srcBalloon);
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon = this.srcBalloon;
+      destBalloon = Object.assign({}, balloot);
     }
     //3.move to limit on P from D
-    if(this.srcBalloon.dark + this.srcBalloon.pale > 13 && this.srcBalloon.pale != 13){
-      console.log("move to limit on P from D 3");
+    if(destBalloon.dark + destBalloon.pale > 13 && destBalloon.pale != 13){
+    //  console.log("move to limit on P from D 3");
       let temp = 0;
       //let temp2 = 0;
       temp = 13 - destBalloon.pale; //checks how much space pale has left
       destBalloon.dark = (destBalloon.dark - temp);
       destBalloon.pale = (destBalloon.pale + temp);
-      this.graph.addEdge(this.srcBalloon, destBalloon);
-      this.listOfBalloons.push(destBalloon);
-      console.log(destBalloon);
-      console.log(this.srcBalloon);
+
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon = this.srcBalloon;
+      destBalloon = Object.assign({}, balloot);
     }
-    /*
 //END OF D AND P mod section ------------------------------------------------------
 
     //this section will be modifying dark and veined
-    //error check
-    if(this.srcBalloon.dark + this.srcBalloon.veined > 19){
-      console.log("error. check dark and veined");
-      console.log(this.srcBalloon.dark);
-      console.log(this.srcBalloon.veined)
+    if(destBalloon.dark + destBalloon.veined > 19){//error
+      console.log("error, check dark and veined");
+      console.log(destBalloon.dark);
+      console.log(destBalloon.veined);
     }
     //1. move all to D from V
-    if(this.srcBalloon.dark + this.srcBalloon.veined <= 19){
-      destBalloon.dark = (this.srcBalloon.dark + this.srcBalloon.veined);//dark = dark + veined
-      destBalloon.veined = (this.srcBalloon.veined - this.srcBalloon.veined);//veined = veined - veined
+    if(destBalloon.dark + destBalloon.veined <= 19){
+      destBalloon.dark = destBalloon.dark + destBalloon.veined;
+      destBalloon.veined = destBalloon.veined - destBalloon.veined;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon.dark = this.srcBalloon.dark;
-      destBalloon.veined = this.srcBalloon.veined;
+      destBalloon = Object.assign({}, balloot);
     }
     //2. move all to V from D
-    if(this.srcBalloon.dark + this.srcBalloon.veined <= 7){
-      destBalloon.veined = (this.srcBalloon.dark + this.srcBalloon.veined);
-      destBalloon.dark = (this.srcBalloon.dark - this.srcBalloon.dark);
+    if(destBalloon.dark + destBalloon.veined <= 7){
+      destBalloon.veined = destBalloon.dark + destBalloon.veined;
+      destBalloon.dark = destBalloon.dark - destBalloon.dark;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon");
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon.dark = this.srcBalloon.dark;
-      destBalloon.veined = this.srcBalloon.veined;
+      destBalloon = Object.assign({}, balloot);
     }
     //3. move to limit on V from D
-    if(this.srcBalloon.dark + this.srcBalloon.veined > 7 && this.srcBalloon.veined != 7){
+    if(destBalloon.dark + destBalloon.veined > 7 && destBalloon.veined != 7){
       let temp = 0;
-      //let temp2 = 0;
-      temp = 7 - this.srcBalloon.veined; //checks how much space pale has left
-      destBalloon.dark = (this.srcBalloon.dark - temp); //dark = dark - temp (dark is the amount subtracted)
-      destBalloon.veined = (this.srcBalloon.veined + temp); //add that to veined
-      this.graph.addEdge(this.srcBalloon, destBalloon);
-      this.listOfBalloons.push(destBalloon);
+      temp = 7 - destBalloon.veined;
+
+      destBalloon.dark = destBalloon.dark - temp;
+      destBalloon.veined = destBalloon.veined + temp;
+
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon.dark = this.srcBalloon.dark;
-      destBalloon.veined = this.srcBalloon.veined;
+      destBalloon = Object.assign({}, balloot);
     }
 //END OF D AND V mod section -------------------------------------------------------
 
     //this section will be modifying pale and veined
     //1. move all to P from V
-    if(this.srcBalloon.pale + this.srcBalloon.veined <= 13){
-      destBalloon.pale = (this.srcBalloon.pale + this.srcBalloon.veined)
-      destBalloon.veined = (this.srcBalloon.veined - this.srcBalloon.veined);
+    if(destBalloon.pale + destBalloon.veined <= 13){
+      destBalloon.pale = destBalloon.pale + destBalloon.veined;
+      destBalloon.veined = destBalloon.veined - destBalloon.veined;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
       //reset balloon
-      destBalloon.veined = this.srcBalloon.veined;
-      destBalloon.pale = destBalloon.pale;
+      destBalloon = Object.assign({}, balloot);
+
     }
     //2. move to LIMIT on P from V
-    else if(this.srcBalloon.pale + this.srcBalloon.veined > 13 && this.srcBalloon.pale != 13){
+    else if(destBalloon.pale + destBalloon.veined > 13 && destBalloon.pale != 13){
       let temp = 0;
-      temp = 13 - this.srcBalloon.pale;
-      destBalloon.veined = (this.srcBalloon.veined - temp); //the amount you're taking from veined
-      destBalloon.pale = (this.srcBalloon.pale + temp);//add that to pale
+      temp = 13 - destBalloon.pale;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);
-      this.listOfBalloons.push(destBalloon);
+      destBalloon.veined = destBalloon.veined - temp;
+      destBalloon.pale = destBalloon.pale + temp;
 
-      //reset balloons
-      destBalloon.veined = this.srcBalloon.veined;
-      destBalloon.pale = this.srcBalloon.pale;
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
+
+      //reset balloon
+      destBalloon = Object.assign({}, balloot);
     }
     //3. move all to V from P
-    if(this.srcBalloon.pale + this.srcBalloon.veined <= 7){
-      destBalloon.veined = (this.srcBalloon.veined + this.srcBalloon.pale);
-      destBalloon.pale = (this.srcBalloon.pale - this.srcBalloon.pale);
+    if(destBalloon.pale + destBalloon.veined <= 7){
+      destBalloon.veined = destBalloon.veined + destBalloon.pale;
+      destBalloon.pale = destBalloon.pale - destBalloon.pale;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
 
-      //reset balloons
-      destBalloon.veined = this.srcBalloon.veined;
-      destBalloon.pale = this.srcBalloon.pale;
+      //reset balloon
+      destBalloon = Object.assign({}, balloot);
     }
     //4. move to LIMIT on V from P
-    else if(this.srcBalloon.pale + this.srcBalloon.veined > 7 && this.srcBalloon.vained != 7){
+    else if(destBalloon.pale + destBalloon.veined > 7 && destBalloon.veined != 7){
       let temp = 0;
-      temp = 7 - this.srcBalloon.veined; // amount you can add to veined
-      destBalloon.pale = (this.srcBalloon.pale - temp);//amount being removed from pale
-      destBalloon.veined = (this.srcBalloon.veined + temp);
+      temp = 7 - destBalloon.veined;
 
-      this.graph.addEdge(this.srcBalloon, destBalloon);//add to graph
-      this.listOfBalloons.push(destBalloon);//add to managers list
+      destBalloon.pale = destBalloon.pale - temp;
+      destBalloon.veined = destBalloon.veined + temp;
 
-      //reset balloons
-      destBalloon.veined = this.srcBalloon.veined;
-      destBalloon.pale = this.srcBalloon.pale;
+      if(destBalloon == balloot){//if new balloon == source balloon dont push
+        console.log("same balloon")
+      }else{//push
+        this.graph.addEdge(balloot, destBalloon);//add to graphs
+        this.listOfBalloons.push(destBalloon);//add to managers list
+        //console.log(destBalloon);
+        //console.log(balloot);
+      }
+
+      //reset balloon
+      destBalloon = Object.assign({}, balloot);
     }
-      */
+    //console.log("got to the end of run")
+
   }//end of run()
 }
